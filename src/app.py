7729913a4091ced708771df2df0a82f7e1a4cff2
LojanArun <lojan13-2002@hotmail.com>
2025-05-26 16:34:42 +0000

    code done i think

--- src/app.py
+++ src/app.py
@@ -1,11 +1,15 @@
+from sqlalchemy.orm import sessionmaker
+
+import controller
+from controller import session 
+
 def main():
     message = """
 ======= CAISSE =======
 1 - Rechercher un produit
-2 - Enregistrer un vente
-3 - Gerer un retour
-4 - Consulter le stock d'un produit
-5 - Exit
+2 - Enregistrer une vente
+3 - Consulter le stock d'un produit
+4 - Exit
 --------------"""
 
     while True:
@@ -13,16 +17,52 @@ def main():
         choice = int(input("Choisissez une option: "))
 
         if choice == 1:
-            pass
+            id = int(input("Mettez l'ID du produit: "))
+            product = controller.get_product_by_id(id) 
+
+            if product:
+                print(f"Name: {product.name}, Description: {product.description}, Nombre en stock: {product.stock}")
+            else:
+                print("Produit non trouvé")
         elif choice == 2:
-            pass
+            lignes_ventes = []
+            while True: 
+                product_id = int(input("Mettez l'id du produit que vous voulez acheter [0 pour quitter]: "))
+                
+                if product_id == 0:
+                    break
+                
+                product = controller.get_product_by_id(product_id)
+
+                if not product:
+                    print("Produit pas trouvé")
+                    continue
+
+                quantite_voulu = int(input("Combien: "))
+                if product.stock - quantite_voulu < 0:
+                    print("Pas assez en stock")
+                    continue
+                    
+                controller.update_stock(product, quantite_voulu)
+                session.add(product)
+                session.commit()
+                ligne_vente = controller.create_line_sale(product, quantite_voulu)
+                        
+                lignes_ventes.append(ligne_vente)
+                        
+
+            sale = controller.create_sale(lignes_ventes, product)
+            session.add(sale)
+            session.commit()
         elif choice == 3:
-            pass
+            id = int(input("Mettez l'ID du produit: "))
+            product =  controller.get_stock(id)
+
+            print(f"Quantite actuel: {product.stock}")
         elif choice == 4:
-            pass
-        elif choice == 5:
             break
         else:
+            print("Mauvaise input")
             continue
 
 if __name__ == "__main__":
--- src/models.py
+++ src/models.py
@@ -1,9 +1,20 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, create_engine 
 
-db_url = 'postgresql+psycopg2://admin:admin@localhost:5432/postgres'
+db_url = 'postgresql+psycopg2://admin:admin@10.194.32.165:5432/postgres'
 engine = create_engine(db_url)
 Base = declarative_base()
+
+class LineSale(Base):
+    __tablename__ = "line_sales" 
+
+    id = Column(Integer, primary_key=True)
+    quantite = Column(Integer)
+    prix = Column(Float)
+
+    #Relationships
+    sale = Column(Integer, ForeignKey("sales.id"))
+    product = Column(Integer, ForeignKey("products.id"))
 
 class Product(Base):
     __tablename__ = "products"
@@ -12,12 +23,22 @@ class Product(Base):
     name = Column(String)
     category = Column(String)
     description = Column(String)
+    prix_unitaire = Column(Float)
+    stock = Column(Integer)
+
+    #Relationships
+    ligne_vente = relationship(LineSale)
 
 
 class Sale(Base):
     __tablename__ = "sales"
 
     id = Column(Integer, primary_key=True)
+    total = Column(Float)
+    user = Column(ForeignKey("users.id"))
+
+    #Relationships
+    line_vente = relationship(LineSale)
 
 class User(Base):
     __tablename__ = "users"
@@ -25,10 +46,9 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
 
-class LineSale(Base):
-    __tablename__ = "line_sales" 
+    #Relationships
+    sale = relationship(Sale)
 
-    id = Columm(Integer, primary_key=True)
 
 # Will create the database + the tables associated with it
 Base.metadata.create_all(engine)

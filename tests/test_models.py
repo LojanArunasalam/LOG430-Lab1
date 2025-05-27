1f3a9622d69f1174d1436d202257d685cc47fa2d
LojanArun <lojan13-2002@hotmail.com>
2025-05-27 23:49:00 +0000

    one last time...

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,22 +1,23 @@
+import pytest
 from src.models import User, Sale, LineSale, Product
-import unittest 
-# Will do test afterwards, for now, I put some template test 
-class TestProduct(unittest.TestCase):
-    def test_product(self):
-        product = Product(name="Timbits",category="Snacks",description="Tiny donut balls",prix_unitaire=0.25,stock=100)
-        self.assertEqual(product.name, "Timbits")
-        self.assertEqual(product.stock, 100)
-        self.assertAlmostEqual(product.prix_unitaire, 0.25)
 
-class TestSale(unittest.TestCase):
-    def test_sale(self):
-        assert True is True
-        
-class TestLineSale(unittest.TestCase):
-    def test_line_sale(self):
-        assert True is True
+def test_product():
+    product = Product(
+        name="Timbits",
+        category="Snacks",
+        description="Tiny donut balls",
+        prix_unitaire=0.25,
+        stock=100
+    )
+    assert product.name == "Timbits"
+    assert product.stock == 100
+    assert product.prix_unitaire == pytest.approx(0.25)
 
+def test_sale():
+    pass
 
-class TestUser(unittest.TestCase):
-    def test_user(self):
-        assert True is True
+def test_line_sale():
+    pass
+
+def test_user():
+    pass
